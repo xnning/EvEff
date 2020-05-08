4ced39fe3d92e0aee702f5786585865018ea496b
Ningning Xie <xnningxie@gmail.com>
2020-05-08 16:22:14 -0700

    more on state test

--- app/TestState.hs
+++ app/TestState.hs
@@ -6,6 +6,7 @@ module TestState where
 
 import Criterion.Main
 import Criterion.Types
+import EffEvScoped
 import Library hiding (main)
 
 import Data.IORef
@@ -52,40 +53,13 @@ countExtEff = do n <- FS.get
 runExtEff n = FS.runState n countExtEff
 
 -------------------------------------------------------
--- IOREF
+-- EFF LCOAL NON TAIL
 -------------------------------------------------------
 
-refCount :: Int -> Int
-refCount init
-  = let l = unsafePerformIO (newIORef init)
-    in runRefCount l
-
-runRefCount :: IORef Int -> Int
-runRefCount r
-  = let x = localGet r
-    in if (x == 0) then x else seq (localSet r (x - 1)) (runRefCount r)
-
-{-# INLINE unsafeIO #-}
-unsafeIO :: IO a -> a
-unsafeIO io = let x = unsafePerformIO io in seq x x
-
-{-# INLINE localGet #-}
-localGet :: IORef a -> a
-localGet r = unsafeIO (readIORef r)
-
-{-# INLINE localSet #-}
-localSet :: IORef a -> a -> ()
-localSet r x = unsafeIO (writeIORef r x)
-
-
-ioRefCount :: Int -> IO Int
-ioRefCount init
-  = do r <- newIORef init
-       ioLoop r
-
-ioLoop r
-  = do i <- readIORef r
-       if (i==0) then return i else do writeIORef r (i - 1); ioLoop r
+lCountNonTail :: Int -> Int
+lCountNonTail n = erun $ lstateNonTail n $
+           do x <- runCount ()
+              return x
 
 -------------------------------------------------------
 -- TESTS
@@ -94,21 +68,20 @@ ioLoop r
 ppure          n = bench "pure"    $ whnf runPure    n
 monadic        n = bench "monadic" $ whnf runMonadic n
 
-effPlain n = bench "eff plain state"   $ whnf count n
-effPar   n = bench "eff parameterized" $ whnf pCount n
-effLo    n = bench "eff local"         $ whnf lCount n
+effPlain n = bench "eff plain state"    $ whnf count n
+effPar   n = bench "eff parameterized"  $ whnf pCount n
+effLo    n = bench "eff local"          $ whnf lCount n
+effLoNt  n = bench "eff local non tail" $ whnf lCountNonTail n
 
-ext    n = bench "extensible effects "       $ whnf runExtEff n
-
-testRefCount   n = bench "refCount"          $ whnf refCount n
-testIORefCount n = bench "ioRefCount"        $ whnfIO (ioRefCount n)
+ext      n = bench "extensible effects "     $ whnf runExtEff n
 
 
 comp n  = [ ppure n
           , monadic n
           , effPlain n
-          , effLo n
           , effPar n
+          , effLo n
+          , effLoNt n
           , ext n ]
 iterExp = 6
 
