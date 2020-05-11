ccaf3e15b84d37395fb8f5906e3a4d392a456286
daan <daanl@outlook.com>
2020-05-11 14:39:27 -0700

    update TestPyth with direct implementation for Eff

--- app/TestPyth.hs
+++ app/TestPyth.hs
@@ -13,10 +13,10 @@ import Criterion.Types
 -- import Library hiding (main)
 
 import Control.Monad (foldM)
-import qualified Control.Monad.State as Ms
-import qualified Control.Monad.Reader as Mr
-import Control.Monad.Cont as Mc
-import GHC.Base
+import qualified Control.Monad.State as Ms hiding (mapM)
+import qualified Control.Monad.Reader as Mr hiding (mapM)
+import Control.Monad.Cont as Mc hiding (mapM)
+import GHC.Base hiding (mapM)
 
 -- Extensible Effects
 import qualified Control.Eff as EE
@@ -73,7 +73,41 @@ pythEEFast n = length $
 -------------------------------------------------------
 -- EFF
 -------------------------------------------------------
+-------------------------------------------------------
+-- EFF
+-------------------------------------------------------
 
+data NDet e ans = NDet { zero :: forall a. Op () a e ans
+                       , pick :: Op Int Int e ans }
+
+
+epyth :: (NDet :? e) => Int -> Eff e (Int, Int, Int)
+epyth upbound = do
+ x <- perform pick upbound
+ y <- perform pick upbound
+ z <- perform pick upbound
+ if (x*x + y*y == z*z) then return (x,y,z) else perform zero ()
+
+-- slow version
+makeChoiceA0 :: Eff (NDet :* e) a -> Eff e [a]
+makeChoiceA0 =  handlerRet (\x -> [x])
+                   NDet{ zero = except (\_ -> return [])
+                       , pick = operation (\hi k -> loop k [] hi)
+                       }
+             where loop k acc 0 = return (concat acc)
+                   loop k acc n = do x <- k n
+                                     loop k (x:acc) (n-1)
+                                 
+             
+
+runPythEff :: Int -> ([(Int,Int,Int)])
+runPythEff n = runEff $ makeChoiceA0 $ epyth n 
+
+testEff = pyth20 == (runPythEff 20)
+
+pythEff n = length $ (runPythEff n)
+
+{-
 data NDet e ans = NDet { zero :: forall a. Op () a e ans
                        , plus :: Op () Bool e ans }
 
@@ -99,33 +133,36 @@ testEff = pyth20 == ((runEff $ makeChoiceA0 $ pyth 20) :: [(Int,Int,Int)])
 
 pythEff n = length $
            ((runEff $ makeChoiceA0 $ pyth n) :: [(Int,Int,Int)])
+-}
+
 
 
 -- do we have a fast version? hmm
-{-
-newtype Q e m a = Q [Eff (Local (Q e m a) :* e) (m a)]
 
-makeChoiceA :: Alternative m => Eff (NDet :* e) a -> Eff e (m a)
-makeChoiceA =  handlerLocalRet (Q []) (\x _ -> pure x) $
-               NDet{ zero = operation (\_ k -> do (Q q) <- localGet
-                                                  case q of
-                                                    (m:ms) -> localPut (Q ms) >> m
-                                                    []     -> return empty
-                                      )
-                   , plus = operation (\_ k -> do (Q q) <- localGet
-                                                  localPut (Q (k False : q))
-                                                  liftM2 (<|>) (k True) (do (Q q) <- localGet
-                                                                            case q of
-                                                                              (m:ms) -> localPut (Q ms) >> m
-                                                                              [] -> return empty))
+newtype Q e a = Q [Eff (Local (Q e a) :* e) [a]]
+
+makeChoiceA :: Eff (NDet :* e) a -> Eff e [a]
+makeChoiceA =  handlerLocalRet (Q []) (\x _ -> [x]) $
+               NDet{ zero = except (\_ -> step)
+                   , pick = operation (\hi k -> do (Q q) <- localGet
+                                                   localPut (Q (map k [1..hi] ++ q))
+                                                   step)
                    }
+                   
+step :: Eff (Local (Q e a) :* e) [a]
+step     = do (Q q) <- localGet
+              case q of
+                (m:ms) -> do localPut (Q ms) 
+                             xs <- m
+                             ys <- step
+                             return (xs ++ ys)
+                []     -> return []
 
-
-testEffFast = pyth20 == ((runEff $ makeChoiceA $ pyth 20) :: [(Int,Int,Int)])
+testEffFast = pyth20 == ((runEff $ makeChoiceA $ epyth 20) :: [(Int,Int,Int)])
 
 pythEffFast n = length $
-                ((runEff $ makeChoiceA $ pyth n) :: [(Int,Int,Int)])
--}
+                ((runEff $ makeChoiceA $ epyth n) :: [(Int,Int,Int)])
+
 
 -------------------------------------------------------
 -- TEST
@@ -135,7 +172,7 @@ comp n = [ bench "monadic"            $ whnf pythMonadic n
          , bench "extensible effects slow" $ whnf pythEESlow n
          , bench "extensible effects fast" $ whnf pythEEFast n
          , bench "eff slow"  $ whnf pythEff n
---         , bench "eff fast"  $ whnf pythEffFast n
+         -- , bench "eff fast"  $ whnf pythEffFast n
          ]
 
 num :: Int
